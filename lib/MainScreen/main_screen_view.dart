d3db2667a196cb388af56cac0e57714e501e80fe
JackobPunch <jakubcios99@gmail.com>
2024-10-06 13:06:11 +0200

    Update main_screen_view.dart

--- lib/MainScreen/main_screen_view.dart
+++ lib/MainScreen/main_screen_view.dart
@@ -14,62 +14,120 @@ class MainScreenViewState extends State<MainScreenView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: ColorScheme.fromSeed(seedColor: Colors.blue).primary,
-        title: const Text(
-          'Panel Sterowania Zadajnikiem v. 0.1',
-          style: TextStyle(color: Colors.white),
+      appBar: const MainAppBar(),
+      body: MainBody(viewModel: viewModel),
+    );
+  }
+}
+
+class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const MainAppBar({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: ColorScheme.fromSeed(seedColor: Colors.blue).primary,
+      title: const Text(
+        'Panel Sterowania Zadajnikiem v. 0.1',
+        style: TextStyle(color: Colors.white),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class MainBody extends StatelessWidget {
+  final MainScreenViewModel viewModel;
+
+  const MainBody({super.key, required this.viewModel});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const DescriptionText(),
+          const SizedBox(height: 20),
+          SpeedDropdown(viewModel: viewModel),
+          const SizedBox(height: 40),
+          SendButton(viewModel: viewModel),
+        ],
+      ),
+    );
+  }
+}
+
+class DescriptionText extends StatelessWidget {
+  const DescriptionText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Text(
+            "Application to control the test signal transmitter. Allows selection of travel speed."),
+        Text('Select the speed of the sender:'),
+      ],
+    );
+  }
+}
+
+class SpeedDropdown extends StatefulWidget {
+  final MainScreenViewModel viewModel;
+
+  const SpeedDropdown({super.key, required this.viewModel});
+
+  @override
+  SpeedDropdownState createState() => SpeedDropdownState();
+}
+
+class SpeedDropdownState extends State<SpeedDropdown> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+      decoration: BoxDecoration(
+        border: Border.all(),
+        borderRadius: BorderRadius.circular(4),
+      ),
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton<Option>(
+          isExpanded: true,
+          value: widget.viewModel.selectedOption,
+          hint: const Text('Select Speed'),
+          onChanged: (Option? newValue) {
+            setState(() {
+              widget.viewModel.updateSelectedOption(newValue);
+            });
+            print('Selected speed: ${newValue?.description}');
+          },
+          items: Option.values.map<DropdownMenuItem<Option>>((Option value) {
+            return DropdownMenuItem<Option>(
+              value: value,
+              child: Text(value.description.toString().split('.').last),
+            );
+          }).toList(),
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-                "Aplikacja do sterowania zadajnikiem sygnału testowego. Pozwala na wybór prędkości jazdy."),
-            const Text('Wybierz prędkość zadajnika:'),
-            const SizedBox(height: 20),
-            Container(
-              width: 200,
-              decoration: BoxDecoration(
-                border: Border.all(
-                    color: Colors.blue, width: 1), // this adds border
-                borderRadius:
-                    BorderRadius.circular(5), // this makes the border rounded
-              ),
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: DropdownButtonHideUnderline(
-                // add this line
-                child: DropdownButton<Option>(
-                  focusColor: Theme.of(context).scaffoldBackgroundColor,
-                  isExpanded: true,
-                  value: viewModel.selectedOption,
-                  onChanged: (Option? newValue) {
-                    setState(() {
-                      viewModel.updateSelectedOption(newValue);
-                    });
-                    print(viewModel
-                        .selectedOption); // Print selected option when changed
-                  },
-                  items: Option.values
-                      .map<DropdownMenuItem<Option>>((Option value) {
-                    return DropdownMenuItem<Option>(
-                      value: value,
-                      child: Text(value.description.toString().split('.').last),
-                    );
-                  }).toList(),
-                ),
-              ),
-            ),
-            const SizedBox(height: 40),
-            ElevatedButton(
-                onPressed: () {
-                  viewModel.onButtonPressed();
-                },
-                child: const Text("Send"))
-          ],
-        ),
-      ),
+    );
+  }
+}
+
+class SendButton extends StatelessWidget {
+  final MainScreenViewModel viewModel;
+
+  const SendButton({super.key, required this.viewModel});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        viewModel.onButtonPressed();
+      },
+      child: const Text("Send"),
     );
   }
 }
